27afcc59be426af4bd691b9981e4015dac2230ca
Kelly Russell <russellk10@nku.edu>
2019-03-07 00:16:17 -0500

    Pixel color change, small layout css work

--- js/app.js
+++ js/app.js
@@ -1,11 +1,20 @@
 var canvas = [];
+// Use an array to store the pixels so they can be output
+var currentColor = "";
 
- $(document).ready(function() {
-  for (var x = 1; x <= 32; x++) {
-    var $pixel = $("<div class='pixel'></div>");
-    $("#pixelCanvas").append($pixel);
+$(document).ready(function() {
+  for (var x = 0; x < 16; x++) {
+    for (var y = 0; y < 16; y++) {
+      var $pixel = $("<div class='pixel'></div>");
+      $("#pixelCanvas").append($pixel);
+    }
   }
- });
+
+  // Click on each pixel to change color
+  $(".pixel").click(function() {
+    $(this).css("background-color", "red");
+  });
+});
 
 //  Will need to figure this out so a set number of pixels w*h
 // Every image will be the same size
