c13c117019f8246926829cf726707a59f9a3271a
kangacopter <kelly.russell@eltoro.com>
2019-03-07 19:58:40 -0500

    Worked on the palette color selection process

--- js/app.js
+++ js/app.js
@@ -12,10 +12,57 @@ $(document).ready(function() {
 
   // Click on each pixel to change color
   $(".pixel").click(function() {
+      // console.log($(this).css("background-color"))
+ 
     $(this).css("background-color", "red");
   });
 });
 
+// Set currentColor, which is a CLASS NAME to apply to the pixel
+// Listen for click on each color, set current color variable to that color
+$(".color").click(function() {
+  console.log(".color clicked")
+  switch ($(this).attr('id')) {
+    case 'color1':
+      console.log("color1 clicked");
+      currentColor = $(this).attr("class")
+
+      // $(this).attr("class").split(' ').each(
+      //   function() {
+      //     console.log(this + "ldkafjds")
+      //   })
+      // May have to use a regex to kill the "color" class and isolate
+      // Or maybe an array and pop color out??
+      console.log(currentColor)
+      break;
+    case 'color2':
+      console.log("color2 clicked");
+      break;
+      case 'color3':
+      console.log("color3 clicked");
+      break;
+    case 'color4':
+      console.log("color4 clicked");
+      break;
+      case 'color5':
+      console.log("color5 clicked");
+      break;
+    case 'color6':
+      console.log("color6 clicked");
+      break;
+      case 'color7':
+      console.log("color7 clicked");
+      break;
+    case 'color8':
+      console.log("color8 clicked");
+      break;
+  }
+})
+
+
 //  Will need to figure this out so a set number of pixels w*h
 // Every image will be the same size
 // Will need an array to store all of the pixels!!!
+
+
+// Maybe don't use .css and set this with classes
