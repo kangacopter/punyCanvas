0865c02ed11e56fcde3bb9d99a1792971c4a825a
Kelly Russell <russellk10@nku.edu>
2019-03-19 23:50:13 -0400

    98 percent proper responsive and looks decent on mobile, tweaked the function for populating pixels to make that easier

--- js/app.js
+++ js/app.js
@@ -3,23 +3,21 @@ let canvas = [];
 let currentColor = "";
 let dragging = false;
 
-// Maybe do something with clicking + drag (so you don't have to click each square)
-// Clear pixel option (maybe eraser in palette)
-// Clear canvas
-
+// Initialize the canvas of pixels
 $(document).ready(function() {
   for (var x = 0; x < 16; x++) {
+    let row = '<div class="pixelRow">';
     for (var y = 0; y < 16; y++) {
-      let $pixel = $("<div class='pixel'></div>");
-      $("#pixelCanvas").append($pixel);
+      row += "<div class='pixel'></div>";
     }
+    row += "</div>";
+    $("#pixelCanvas").append(row);
   }
 
   // Click on each pixel to change color. Set dragging function so user can
   // hold the mouse down and drag over elements to change them.
   $(".pixel").on("click", function() {
     $(this).removeClass("red green blue yellow purple orange black white");
-    // Check this out ^^ must only remove the color but not the actual color name class.
     $(this).addClass(currentColor);
   });
 
@@ -102,7 +100,3 @@ $(".color").on("click", function() {
     );
   });
 });
-
-//  Will need to figure this out so a set number of pixels w*h
-// Every image will be the same size
-// Will need an array to store all of the pixels!!!
