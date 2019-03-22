74b7dc51bae51cbcbe926dabb89562de024ce4ce
kangacopter <kelly.russell@eltoro.com>
2019-03-21 20:00:18 -0400

    Beginning to do the HARD STUFF, planning

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-let canvas = [];
+let pixels = [];
 // Use an array to store the pixels so they can be output
 let currentColor = "";
 let dragging = false;
@@ -13,7 +13,6 @@ $(document).ready(function() {
     row += "</div>";
     $("#pixelCanvas").append(row);
   }
-
 
   // Click on each pixel to change color. Set dragging function so user can
   // hold the mouse down and drag over elements to change them.
@@ -31,14 +30,12 @@ $(document).ready(function() {
   });
 
   // ATTEMPT TO FIX DRAGGING PROBLEM BUT IT IS FLAWED
-    $(document).on("dragend", function() {
-      dragging = false;
-    });
-    $(document).on("dragstart", function() {
-      dragging = false;
-    });
-
-    // TEST THIS SOME MORE
+  $(document).on("dragend", function() {
+    dragging = false;
+  });
+  $(document).on("dragstart", function() {
+    dragging = false;
+  });
 
   // Hovering over pixels to paint if mouse is clicked
   $(".pixel").on("mouseover", function() {
@@ -110,4 +107,50 @@ $(".color").on("click", function() {
       "red green blue yellow purple orange black white none"
     );
   });
+
+  // Function for collecting each pixel color
+  // This should be called when the output button is hit
 });
+
+// Creates image output
+
+// for (var x = 0; x < 16; x++) {
+//   let row = '<div class="pixelRow">';
+//   for (var y = 0; y < 16; y++) {
+//     row += "<div class='pixel'></div>";
+//   }
+//   row += "</div>";
+//   $("#pixelCanvas").append(row);
+// }
+
+// Go through each row, and based on the class, add the pixel's color class to the array in string form
+// Or maybe based on that class, ADD THE CORRECT RGB
+// So the array of numbers will be:
+// [
+//   [red, green, blue], // format
+//   [60, 145, 230], // blue
+//   [159, 211, 86], // green
+//   [91, 80, 122], // purple
+//   [244, 92, 9], // orange
+//   [214, 40, 40], // red
+//   [91, 202, 58], // yellow
+//   [255, 255, 255], // white
+//   [255, 255, 255], // black
+// ]
+// Maybe I can have set arrays for these values, then insert the arrays into the array
+// in order based on what the class color is... OHHHHH yes
+
+
+// Set color arrays
+let blue = [60, 145, 230];
+let green = [159, 211, 86];
+let purple = [91, 80, 122];
+let orange = [244, 92, 9];
+let red = [214, 40, 40];
+let yellow = [91, 202, 58];
+let white = [255, 255, 255];
+let black = [255, 255, 255];
+
+// Determine which class is equivalent to which array of the same name
+
+// Add that array to the pixel array
